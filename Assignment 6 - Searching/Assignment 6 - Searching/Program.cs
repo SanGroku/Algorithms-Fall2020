dc14979c2842ebd25a98ac815347e51c7ca807fc
SanGroku <37126121+SanGroku@users.noreply.github.com>
2020-12-05 23:24:28 -0600

    Add Assignment
    Adds assignment 6 - searching, to the repo.

---
+++ Assignment 6 - Searching/Assignment 6 - Searching/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Assignment_6___Searching
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
